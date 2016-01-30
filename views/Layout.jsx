8e804f472873331d649386785cfa03416e1deb9f
David Lakata <davidlakata@gmail.com>
2016-01-30 13:06:49 -0500

    remove ga from console

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -60,7 +60,7 @@ var Layout = React.createClass({
         </div>
       </div>
       <Footer {...this.props}/>
-      <script type="text/javascript" src="/js/ga.js"></script>
+      {!this.props.inConsole ? <script type="text/javascript" src="/js/ga.js"></script> : null }
       <script type="text/javascript" src="/js/page.js"></script>
       </body>
       </html>
