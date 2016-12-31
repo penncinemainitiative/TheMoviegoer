d829e22e497bf10a31c8b1dc30c90df12a5b18e8
David Lakata <davidlakata@gmail.com>
2016-12-31 15:57:53 -0500

    add sass to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const path = require('path');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 var spawn = require('child_process').spawn, node;
 
@@ -23,7 +26,10 @@ const paths = {
   common: 'src/common/**/*.js',
   server: 'src/server/**/*.js',
   views: 'views/**/*',
-  static: 'static/**/*',
+  fonts: 'static/fonts/**/*',
+  images: 'static/images/**/*',
+  js: 'static/js/**/*',
+  sass: 'static/sass/**/*',
   public: 'public',
   dist: 'dist'
 };
@@ -35,7 +41,7 @@ gulp.task('server', function () {
     .on('end', runServer);
 });
 
-gulp.task('prod-server', function() {
+gulp.task('build-server', function() {
   return gulp.src(paths.src)
     .pipe(babel())
     .pipe(gulp.dest(paths.dist));
@@ -46,15 +52,40 @@ gulp.task('views', function () {
     .pipe(gulp.dest(paths.dist + "/server/views"));
 });
 
-gulp.task('static', function () {
-  return gulp.src(paths.static)
-    .pipe(gulp.dest(paths.public));
+gulp.task('js', function () {
+  return gulp.src(paths.js)
+    .pipe(gulp.dest(paths.public + "/js"));
+});
+
+gulp.task('images', function () {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest(paths.public + "/images"));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest(paths.public + "/fonts"));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.sass)
+    .pipe(sass({
+      includePaths: require('node-bourbon').includePaths
+    }))
+    .pipe(cleanCSS())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest(paths.public + "/css"));
 });
 
 gulp.task('watch', function () {
   gulp.watch(paths.views, ['views']);
   gulp.watch(paths.static, ['static']);
+  gulp.watch(paths.sass, ['sass']);
   gulp.watch([paths.common, paths.server], ['server']);
 });
+
+gulp.task('static', ['js', 'images', 'fonts', 'sass']);
+
+gulp.task('prod-server', ['views', 'static', 'build-server']);
 
 gulp.task('default', ['views', 'static', 'watch', 'server']);
