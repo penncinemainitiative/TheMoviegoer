135d57744415eb6b2056f6eed8f1de1e09c785cc
grewp <bpkraken@gmail.com>
2017-01-02 19:08:36 -0500

    implemented nav bar; started sass architecture; started cleaning up repo

--- src/common/components/Header.js
+++ src/common/components/Header.js
@@ -1,12 +1,11 @@
 import React from "react"
-import Row from "react-bootstrap/lib/Row"
 import Link from "react-router/lib/Link"
 import browserHistory from "react-router/lib/browserHistory"
 
 class HeaderItem extends React.Component {
   render() {
     return (
-      <div className='headerItem col-lg-1 col-md-1 col-sm-1 col-xs-2'>
+      <div className='nav_item'>
         <h5><Link to={this.props.url}>{this.props.name}</Link></h5>
       </div>
     );
@@ -48,37 +47,27 @@ export default class Header extends React.Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: '#2b292b',
-      color: '#c29d52',
-      minHeight: '4em',
-      width: '100%'
-    };
-    const logoStyle = {
-      height: '42px',
-      marginTop: '15px',
-      marginBottom: '12px'
-    };
     const items = [
-      {name: 'Login', url: '/login'},
-      {name: 'Console', url: '/console'},
       {name: 'Articles', url: '/articles'},
       {name: 'Writers', url: '/writers'},
       {name: 'About', url: '/about'}
     ];
     return (
-      <Row style={style}>
+      <div className="nav">
         <Link to="/"
-              className="col-lg-1 col-md-2 col-sm-2 col-xs-3 col-lg-offset-2 col-md-offset-1 col-sm-offset-1">
-          <img src="/public/images/logo.png" style={logoStyle}
-               alt="The Moviegoer"/></Link>
-        {items.map((item) => {
-          return <HeaderItem {...item} key={item.name}/>;
-        })}
-        <div className='searchBar col-lg-3 col-md-3 col-sm-3 col-xs-8 col-lg-offset-0 col-md-offset-0 col-sm-offset-0'>
-          <select id="search" className="form-control"/>
+                className="logo">
+            <img src="/public/images/moviegoer_black.png"
+                 alt="The Moviegoer"/></Link>
+        <div className="container">
+          
+          {items.map((item) => {
+            return <HeaderItem {...item} key={item.name}/>;
+          })}
+          <div className='search_bar'>
+            <select id="search" className="form-control"/>
+          </div>
         </div>
-      </Row>
+      </div>
     );
   }
 }
