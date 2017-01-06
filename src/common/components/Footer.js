d08d90f38c120dc35b0bcb43a911af387b502080
grewp <bpkraken@gmail.com>
2017-01-06 13:09:04 -0500

    Finished footer

--- src/common/components/Footer.js
+++ src/common/components/Footer.js
@@ -9,12 +9,16 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <p>The Moviegoer is a student-run blog dedicated to film appreciation 
-        - posting film analyses, reviews, previews, and all things related. If you 
-        would like to work with us, please find out more under the about tab. 
-        The Moviegoer is generously sponsored by the The Kelly Writers House at 
-        the University of Pennsylvania.</p>
-        <Link to="/console">Console</Link>
+        <div className="text-container">
+          <p>The Moviegoer is a student-run blog dedicated to film appreciation 
+          - posting film analyses, reviews, previews, and all things related. If you 
+          would like to work with us, please find out more under the about tab. 
+          The Moviegoer is generously sponsored by the The Kelly Writers House at 
+          the University of Pennsylvania.</p>
+          <p><img src="/public/images/facebook-logo.png"/></p>
+          <p><span>Webmaster: dlakata (at) seas (dot) upenn (dot) edu</span></p>
+          <p><span><Link to="/console">Go to Author Console</Link></span></p>
+        </div>
       </div>
     );
   }
