01029a151abc600880d8772b1150f6a057281453
grewp <bpkraken@gmail.com>
2017-01-10 12:59:03 -0500

    added staff writers, fixed article body images, misc. stylesheet cleanups

--- src/common/components/About.js
+++ src/common/components/About.js
@@ -24,19 +24,34 @@ export default class About extends React.Component {
         <Helmet title="About"/>
         <div className="content-left">
           <div className="letter">
-            <h3>Letter from the Editor</h3>
+            <h3>About</h3>
+            <p>The Moviegoer is a student-run blog dedicated 
+            to film appreciation - posting film analyses, reviews, previews, 
+            and all things related. If you would like to work with us, 
+            please find out more under the about tab. The Moviegoer is 
+            generously sponsored by the The Kelly Writers House at the University 
+            of Pennsylvania.</p>
           </div>
           <div className="contact">
             <h3>Contact Us</h3>
+            <p>If you would like to work with The Moviegoer, 
+            either by writing for us or by aiding in web development, 
+            please contact our Editor-in-Chief, Brad Pettigrew 
+            (bradpett at sas dot upenn).</p>
           </div>
         </div>
         <div className="staff">
           <h3>Staff</h3>
           {staff.map((writer) => {
             return (
-              <p key={writer.name}>
-                <b>{writer.position}</b>: {writer.name}
-              </p>
+                <div key={writer.name} className="author_card">
+                  <div className="image-wrapper">
+                    <div className="inner-wrapper"><img src={writer.image}/></div>
+                  </div>
+                  <div className="text-wrapper">
+                    <p><b>{writer.position}</b>: {writer.name}</p>
+                  </div>
+                </div>
             );
           })}
         </div>
--- src/common/components/Article.js
+++ src/common/components/Article.js
@@ -15,6 +15,13 @@ import ArticleView from "../components/ArticleView"
   state => ({token: state.token})
 )
 export default class Article extends React.Component {
+  componentDidMount() {
+    //use jQuery for more granular styling of article body
+    console.log('test');
+    console.log($('p:has(img)'));
+    $('p:has(img)').addClass('image-container');
+  }
+
   render() {
     return <ArticleView {...this.props}/>
   }
